66fe55e3f532456d76738e1601627473c153379e
ladykabbz <kabiinicole@gmail.com>
2023-05-11 13:09:25 +0300

    submit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-var navbar = document.querySelector('.navigation')
+var navbar = document.querySelector(".navigation");
 // var nav = document.getElementsByClassName('.navItem')
 
-window.onscroll = function() {
-
+window.onscroll = function () {
   // pageYOffset or scrollY
   if (window.pageYOffset > 90) {
-    navbar.classList.add('scrolled');
+    navbar.classList.add("scrolled");
   } else {
-    navbar.classList.remove('scrolled');
+    navbar.classList.remove("scrolled");
   }
-  
 };
+
+document.getElementById("book").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevents the form from being submitted
+
+  // Get the value of the name input field
+  var name = document.getElementById("name").value;
+
+  // Display an alert message including the name
+  alert("Thank you, " + name + ", we will get back to you soon❤");
+
+  // Optionally, you can add code here to perform additional actions or submit the form data to a server
+});
